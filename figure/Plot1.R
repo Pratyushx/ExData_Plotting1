385f955209b1d697d3cb7c822f01c9fae7474deb
Pratyush Sharma <sharmapratyush_bt2k16@dtu.ac.in>
2018-01-15 12:50:31 +0530

    Update Plot1.R

--- figure/Plot1.R
+++ figure/Plot1.R
@@ -28,3 +28,7 @@ t$dateTime <- as.POSIXct(dateTime)
  ## Create the histogram
   hist(t$Global_active_power, main="Global Active Power", xlab = "Global Active Power (kilowatts)", col="red")
 
+  ## Save file and close device
+  #dev.copy(png,"plot1.png", width=480, height=480)
+  #dev.off()
+
